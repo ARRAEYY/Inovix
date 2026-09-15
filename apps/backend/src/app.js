efe49c619229aeac1ee4d6c0eee71b7387869158
Adil <adilreyazsk@gmail.com>
2026-09-16 00:36:49 +0530

    initialize project nosh

---
+++ apps/backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health',(req,res)=>{
+    res.send({
+  "success": true,
+  "message": "Nosh API is running"
+})
+})
+
+module.exports = app;
